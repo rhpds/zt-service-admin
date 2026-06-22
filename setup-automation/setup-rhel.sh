0b0496238c59a71f2ada3b19620fc0ae294af45e
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-06-22 12:05:31 -0400

    Fix setup-rhel.sh comment placement

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This setup is a no-op, no original setup needed
 # Unregister and register the VM
 subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
 
+# This setup is a no-op, no original setup needed
 echo "DONE" >> /root/post-run.log
