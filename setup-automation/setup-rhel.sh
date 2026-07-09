6272778530b9f67a73b1efa8e7ede0a102df27ad
baraka <126705717+Brillar0101@users.noreply.github.com>
2026-07-09 16:44:19 -0400

    Service + packages added

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,5 +5,10 @@ dnf -y remove katello-ca-consumer-*
 subscription-manager clean
 subscription-manager register --activationkey=$ACTIVATION_KEY --org=$ORG_ID --force
 
-# This setup is a no-op, no original setup needed
+# Install required packages
+dnf install -y nfs-utils
+
+# Start rhsm service
+systemctl start rhsm.service
+
 echo "DONE" >> /root/post-run.log
